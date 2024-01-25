4dec7975f959b89b9932b9b512d055ea5a123dfe
luanbt21 <luanbt21@gmail.com>
2024-01-25 09:00:20 +0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let text = `1		Hóa chất Foocmalin	Kg
+2		Hypochlorite 70% TQ	Kg
+3		25% GARLICIN POWDER +STAR	Kg
+4		HLK-101  (500 g* 20 gói / thùng	Kg`
+`
+</script>
+
+<main>
+  <div class="w-full">
+    <div class="flex flex-row justify-evenly">
+      <label for="">amount</label>
+      <label for="">description</label>
+      <label for="">id</label>
+    </div>
+    <textarea name="data" id="data" cols="100" rows="30"> </textarea>
+  </div>
+</main>
